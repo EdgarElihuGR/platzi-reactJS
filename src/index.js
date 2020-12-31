c83eed60e991ccd04102df2aa97b3cfa82075f92
Edgar Elihu García Ramírez <edgar.elihu.garcia.ramirez@gmail.com>
2020-12-31 11:10:10 -0600

    Avance|Forma 1 para crear componente Header y mandar el HTML a la vista

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
-import HolaMundo from './components/HolaMundo'
-import Button from './components/Button'
+import Header from './components/Header'
 
-ReactDOM.render(<Button text="Enviar"/>,document.getElementById("app"))
+ReactDOM.render(<Header/>,document.getElementById("app"))
